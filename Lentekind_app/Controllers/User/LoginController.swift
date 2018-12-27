482f184001e07198837996916c9804e406182d42
Nicolaas Leenknegt <nicolaas.leenknegt.w1123@student.hogent.be>
2018-12-27 23:09:25 +0100

    PodFile aangemaakt en login en registratie scherm aangemaakt

--- Lentekind_app/Controllers/User/LoginController.swift
+++ Lentekind_app/Controllers/User/LoginController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    let emailRegex = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    
+    @IBAction func onLoginClick(_ sender: Any) {
+        guard let email = emailTxt.text, let password = passwordTxt.text else { return }
+        guard !email.isEmpty, !password.isEmpty, emailRegex.evaluate(with: email) else { return }
+        
+        performSegue(withIdentifier: "AdminSegue", sender:  nil)
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-Lentekind_app/Pods-Lentekind_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lentekind_app : NSObject
+@end
+@implementation PodsDummy_Pods_Lentekind_app
+@end
